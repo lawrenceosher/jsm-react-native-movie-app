50669f81cde7e0fbeacabb51d29aa241d1210010
Lawrence Osher <lao2205@gmail.com>
2025-10-20 14:41:36 -0400

    Implemented search screen

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,9 +4,11 @@ import { Image, TextInput, View } from "react-native";
 interface Props {
   placeholder: string;
   onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-export default function SearchBar({ onPress, placeholder }: Props) {
+export default function SearchBar({ onPress, placeholder, value, onChangeText }: Props) {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -18,10 +20,10 @@ export default function SearchBar({ onPress, placeholder }: Props) {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#A8B5DB"
-        className="flex-1 ml-2"
+        className="flex-1 ml-2 text-white"
       />
     </View>
   );
