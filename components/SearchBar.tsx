e7d9d4daf39181daed8f0e560efe9122411e6476
Lawrence Osher <lao2205@gmail.com>
2025-10-20 13:13:14 -0400

    finished setup home screen

---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+export default function SearchBar({ onPress, placeholder }: Props) {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#A8B5DB"
+        className="flex-1 ml-2"
+      />
+    </View>
+  );
+}
